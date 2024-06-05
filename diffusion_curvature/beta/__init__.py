8e093b76d46d70444490370f61e7807d37cce785
Kincaid MacDonald <dev@riddle.press>
2024-06-04 20:25:30 -0600

    removed do not edit warnings on code
    Signed-off-by: Kincaid MacDonald <dev@riddle.press>

---
+++ diffusion_curvature/beta/__init__.py
@@ -0,0 +1,4 @@
+"""
+Stuff here isn't ready for prime time.
+This is where experimental new methods start out, ideally being as unobtrusive as possible -- using 'patches' to the main classes, allowing functions to be inserted only when the corresponding file in the beta folder is imported.
+"""
